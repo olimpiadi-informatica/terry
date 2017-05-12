a4f05a4fadf8148c25bbfc96e47993607191f493
William Di Luigi <williamdiluigi@gmail.com>
2017-05-12 22:32:02 +0200

    Fixed missing font-awesome CSS

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import AppView from './AppView';
 import './index.css';
 import { HashRouter, Route, Link } from 'react-router-dom';
+import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
   <HashRouter>
