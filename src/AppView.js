5b1e9757628284a5b9d0d94e27a7644e2d07ad93
Massimo Cairo <cairomassimo@gmail.com>
2017-05-12 12:56:54 +0200

    Implement login/logout using cookie storage.

--- src/AppView.js
+++ src/AppView.js
@@ -9,22 +9,24 @@ class AppView extends Component {
     super(props);
 
     this.model = new Model({"view": this});
+
     this.model.loadContest();
+    this.model.maybeLoadUser();
   }
 
   render() {
-    if (this.model.isContestLoaded()) {
-      if(this.model.isLoggedIn()) {
-        return <ContestView model={this.model}/>;
-      } else {
-        return <LoginView model={this.model}/>;
-      }
-    } else {
-      return (
-        <div>Loading...</div>
-      );
-    }
+    if (!this.model.isContestLoaded()) return <div>Loading contest...</div>;
+    if (!this.model.isLoggedIn()) return <LoginView model={this.model}/>;
+    if (!this.model.isUserLoaded()) return <div>Loading user...</div>;
+
+    return (
+      <div>
+        <ContestView model={this.model}/>
+        <button onClick={() => this.model.logout()}>Log out</button>
+      </div>
+    );
   }
+
 }
 
 export default AppView;
