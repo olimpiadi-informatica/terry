17330a9c5318e139bb1ad738720390e87bb90a76
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2017-05-12 22:18:49 +0200

    Added some comments

--- src/handlers/info_handler.py
+++ src/handlers/info_handler.py
@@ -17,6 +17,9 @@ from werkzeug.exceptions import Forbidden
 class InfoHandler(BaseHandler):
 
     def get_contest(self, route_args, request):
+        """
+        GET /contest
+        """
         start_timestamp = Database.get_meta("start_time", type=int)
         start_datetime = datetime.fromtimestamp(start_timestamp) if start_timestamp is not None else None
         now = datetime.now()
@@ -32,6 +35,9 @@ class InfoHandler(BaseHandler):
         }
 
     def get_input(self, route_args, request):
+        """
+        GET /input/<id>
+        """
         input_id = route_args["id"]
 
         input_file = Database.get_input(id=input_id)
@@ -40,6 +46,9 @@ class InfoHandler(BaseHandler):
         return BaseHandler.format_dates(input_file)
 
     def get_output(self, route_args, request):
+        """
+        GET /output/<id>
+        """
         output_id = route_args["id"]
 
         output_file = Database.get_output(id=output_id)
@@ -59,6 +68,9 @@ class InfoHandler(BaseHandler):
         return BaseHandler.format_dates(result)
 
     def get_source(self, route_args, request):
+        """
+        GET /source/<id>
+        """
         source_id = route_args["id"]
 
         source_file = Database.get_source(id=source_id)
@@ -68,12 +80,18 @@ class InfoHandler(BaseHandler):
         return BaseHandler.format_dates(source_file)
 
     def get_submission(self, route_args, request):
+        """
+        GET /submission/<id>
+        """
         submission_id = route_args["id"]
 
         submission = Database.get_submission(submission_id)
         return BaseHandler.format_dates(InfoHandler._parse_submission(submission))
 
     def get_user(self, route_args, request):
+        """
+        GET /user/<token>
+        """
         token = route_args["token"]
 
         user = Database.get_user(token)
@@ -104,6 +122,9 @@ class InfoHandler(BaseHandler):
         return BaseHandler.format_dates(user, fields=["date", "contest_end"])
 
     def get_submissions(self, route_args, request):
+        """
+        GET /user/<token>/submissions/<task>
+        """
         token = route_args["token"]
         task = route_args["task"]
 
@@ -114,6 +135,11 @@ class InfoHandler(BaseHandler):
 
     @staticmethod
     def _parse_submission(submission):
+        """
+        Given a submission from a SQL query with some JOIN create a dict by splitting the keys using _
+        :param submission: A dict with the submission
+        :return: A dict with some properties nested
+        """
         result = {}
 
         for k,v in submission.items():
