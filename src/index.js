31906bc3e843f9c7fc41d1155ac9f59f62a87ae3
William Di Luigi <williamdiluigi@gmail.com>
2017-05-13 22:22:30 +0200

    Better handling

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'font-awesome/css/font-awesome.min.css';
 
+/******** DEVELOPMENT SPECIFIC **********/
+if (window.location.origin.endsWith("3000")) location.replace("http://localhost:5050");
+/******** DEVELOPMENT SPECIFIC **********/
+
 ReactDOM.render(
   <Router>
     <AppView />
