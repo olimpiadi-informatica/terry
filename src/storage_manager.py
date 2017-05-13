6bc0472e771ed216215a9d775767374a91f50e23
Luca Versari <veluca93@gmail.com>
2017-05-13 11:37:55 +0200

    Use subdirectories to reduce filesystem issues

--- src/storage_manager.py
+++ src/storage_manager.py
@@ -25,7 +25,8 @@ class StorageManager:
         :return: A path relative to the Config.storedir where save that file
         """
         filename = StorageManager._sanitize(filename)
-        return os.path.join("source", source_id, filename)
+        path_prefix = [source_id[:2], source_id[2:4]]
+        return os.path.join("source", *path_prefix, source_id, filename)
 
     @staticmethod
     def new_output_file(output_id, filename):
@@ -36,7 +37,8 @@ class StorageManager:
         :return: A path relative to the Config.storedir where save that file
         """
         filename = StorageManager._sanitize(filename)
-        return os.path.join("output", output_id, filename)
+        path_prefix = [output_id[:2], output_id[2:4]]
+        return os.path.join("output", *path_prefix, output_id, filename)
 
     @staticmethod
     def new_input_file(input_id, task_name, attempt):
@@ -49,7 +51,8 @@ class StorageManager:
         :return: A path relative to the Config.storedir where that file is
         """
         filename = task_name + "_input_" + str(attempt) + ".txt"
-        filename = os.path.join("input", input_id, filename)
+        path_prefix = [input_id[:2], input_id[2:4]]
+        filename = os.path.join("input", *path_prefix, input_id, filename)
         StorageManager._create_dir(StorageManager.get_absolute_path(filename))
         return filename
 
