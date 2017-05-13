bb3338b0807ed438dddc2389b9fb09ecf7539676
William Di Luigi <williamdiluigi@gmail.com>
2017-05-13 21:51:57 +0200

    Test: avoid installing 'Allow origin' chrome extension

---
+++ proxy.js
@@ -0,0 +1,30 @@
+var http = require('http'),
+    httpProxy = require('http-proxy');
+
+//
+// Create a proxy server with custom application logic
+//
+var proxy = httpProxy.createProxyServer({});
+
+//
+// Create your custom server and just call `proxy.web()` to proxy
+// a web request to the target passed in the options
+// also you can use `proxy.ws()` to proxy a websockets request
+//
+var server = http.createServer(function(req, res) {
+  if (req.url.startsWith("/api/")) {
+    console.log("API: " + req.url);
+    req.url = req.url.slice(4);  // remove "/api"
+    proxy.web(req, res, { target: 'http://127.0.0.1:1234' });
+  } else if (req.url.startsWith("/files/")) {
+    console.log("FILES: " + req.url);
+    req.url = req.url.slice(6);  // remove "/files"
+    proxy.web(req, res, { target: 'http://127.0.0.1:1235' });
+  } else {
+    console.log("SITO: " + req.url);
+    proxy.web(req, res, { target: 'http://127.0.0.1:3000' });
+  }
+});
+
+console.log("listening on port 5050")
+server.listen(5050);
--- src/TerryClient.js
+++ src/TerryClient.js
@@ -2,17 +2,17 @@ import axios from 'axios';
 
 class Client {
   constructor() {
-    this.apiBaseURI = process.env.REACT_APP_API_BASE_URI || "http://localhost:1234/";
+    this.apiBaseURI = process.env.REACT_APP_API_BASE_URI || "http://localhost:5050/api/";
     this.api = axios.create({
       baseURL: this.apiBaseURI,
     });
 
-    this.filesBaseURI = process.env.REACT_APP_FILES_BASE_URI || "http://localhost:1235/";
+    this.filesBaseURI = process.env.REACT_APP_FILES_BASE_URI || "http://localhost:5050/files/";
     this.files = axios.create({
       baseURL: this.filesBaseURI,
     });
 
-    this.statementsBaseURI = process.env.REACT_APP_STATEMENTS_BASE_URI || "http://localhost:1234/";
+    this.statementsBaseURI = process.env.REACT_APP_STATEMENTS_BASE_URI || "http://localhost:5050/api/";
     this.statements = axios.create({
       baseURL: this.statementsBaseURI,
     });
