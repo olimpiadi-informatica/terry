72cf1c389badb04acc87e757bc8886f152806bcb
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2017-09-25 21:39:52 +0200

    Added simple homepage that displays some contest information and a quick-start guide

--- src/ContestView.js
+++ src/ContestView.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Link, NavLink, Route } from 'react-router-dom';
 import TaskView from './TaskView';
 import Countdown from './Countdown';
+import ReactMarkdown from 'react-markdown';
+import moment from 'moment';
+import 'moment-precise-range-plugin';
 
 export default class ContestView extends Component {
   constructor(props) {
@@ -72,6 +75,27 @@ export default class ContestView extends Component {
     );
   }
 
+  render_index() {
+    // start and end are rounded to minutes
+    const start = moment(this.model.contest.data.start_time).seconds(0);
+    const end = moment().add(this.model.user.remaining_time, 'seconds').seconds(0);
+    const length = moment.preciseDiff(start, end);
+
+    return <div>
+      <h1>{this.model.contest.data.name}</h1>
+      <ReactMarkdown source={this.model.contest.data.description}/>
+      <hr />
+      <h2>Usage guide</h2>
+      <p>On the left side of this page you can find the tasks, click on any one to open it.</p>
+      <p>You can submit <em>as many times as you want</em>, but you will have a different input every time. When you
+        make a submission remember to send the correct source file and the output corresponding to the last
+        generated input. When you have uploaded your files <em>remember to submit</em> them by clicking the green
+        button!</p>
+      <p>If you want to submit more than one source code file, please create a zip file containing them.</p>
+      <p>This contest is {length} long.</p>
+    </div>
+  }
+
   render() {
     return (
       <div >
@@ -85,6 +109,7 @@ export default class ContestView extends Component {
               <Route path={'/:taskName'} render={ ({match}) =>
                   <TaskView key={match.params.taskName} model={this.model} taskName={match.params.taskName}/>
               }/>
+              <Route exact path={'/'} render={({match}) => this.render_index()}/>
             </main>
           </div>
         </div>
