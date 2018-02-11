08a7ca3a12c5c890fc943db2e0d98062621e8c54
Massimo Cairo <cairomassimo@gmail.com>
2018-02-11 21:28:24 +0100

    Drop contest duration from home.

--- src/views/ContestView.js
+++ src/views/ContestView.js
@@ -31,7 +31,6 @@ class ContestView extends Component {
         You can submit <em>as many times as you want</em>, but you will have a different input every time. When you make a submission remember to send the correct source file and the output corresponding to the last generated input. When you have uploaded your files <em>remember to submit</em> them by clicking the green button!
       </Trans>
       <p>{t("homepage.guide.part3")}</p>
-      <p>{t("homepage.guide.part4", {length: "TODO: contest duration"})}</p>
     </React.Fragment>
   }
 
--- src/views/IndexView.js
+++ src/views/IndexView.js
@@ -22,7 +22,6 @@ class IndexView extends Component {
         You can submit <em>as many times as you want</em>, but you will have a different input every time. When you make a submission remember to send the correct source file and the output corresponding to the last generated input. When you have uploaded your files <em>remember to submit</em> them by clicking the green button!
       </Trans>
       <p>{t("homepage.guide.part3")}</p>
-      <p>{t("homepage.guide.part4", {length: "TODO: duration"})}</p>
     </React.Fragment>
   }
 }
