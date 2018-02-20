4d4a17c1e43f3a49034fee4104358cc8ef525ba9
Massimo Cairo <cairomassimo@gmail.com>
2018-02-20 15:06:16 +0100

    Handle missing promise in PromiseView (hack!)

--- src/PromiseView.js
+++ src/PromiseView.js
@@ -11,22 +11,25 @@ class PromiseView extends Component {
     if(!props.renderFulfilled || !props.renderPending || !props.renderRejected) throw Error();
   }
 
+  // FIXME: promise should always be present, check and fix!
+
   componentDidMount() {
-    this.props.promise.pushObserver(this);
+    this.props.promise && this.props.promise.pushObserver(this);
   }
 
   componentWillUnmount() {
-    this.props.promise.popObserver(this);
+    this.props.promise && this.props.promise.popObserver(this);
   }
 
   componentWillReceiveProps(newProps) {
     if(newProps.promise !== this.props.promise) {
-      this.props.promise.popObserver(this);
-      newProps.promise.pushObserver(this);
+      this.props.promise && this.props.promise.popObserver(this);
+      newProps.promise && newProps.promise.pushObserver(this);
     }
   }
 
   render() {
+    if(!this.props.promise) return null;
     if(this.props.promise.isPending()) return this.props.renderPending();
     if(this.props.promise.isFulfilled()) return this.props.renderFulfilled(this.props.promise.value);
     if(this.props.promise.isRejected()) return this.props.renderRejected(this.props.promise.error);
