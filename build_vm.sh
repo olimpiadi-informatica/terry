5e29b8a69299bcde4fba143f61ca14e4f8ae8e35
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2018-02-20 18:51:33 +0100

    Added version parameter to build the vm

--- build_vm.sh
+++ build_vm.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -e
 
-[ -z "$5" ] && echo "Usage: $0 root_password target_file image_name disk_size_mb ram_size_mb"
-[ -z "$5" ] && exit 1
+[ -z "$6" ] && echo "Usage: $0 root_password version target_file image_name disk_size_mb ram_size_mb"
+[ -z "$6" ] && exit 1
 
 TMP=$(mktemp -d)
 ROOT_PASSWORD=$1
-TARGET_FILE=$2
-IMAGE_NAME=$3
-DISK_SIZE_MB=$4
-RAM_SIZE_MB=$5
+VERSION=$2
+TARGET_FILE=$3
+IMAGE_NAME=$4
+DISK_SIZE_MB=$5
+RAM_SIZE_MB=$6
 
 
 if [ ${TARGET_FILE: -4} != '.ova' ]
@@ -33,7 +34,7 @@ popd
 sudo bash -e <<EOF
 chmod o+rx ${TMP}
 $(dirname $0)/vm-utils/gen_arch_root.sh ${TMP} ${ROOT_PASSWORD}
-$(dirname $0)/vm-utils/prepare_terry_arch.sh ${TMP}
+$(dirname $0)/vm-utils/prepare_terry_arch.sh ${TMP} ${VERSION}
 $(dirname $0)/vm-utils/gen_image.sh -r ${TMP} -o ${TARGET_FILE} -t ovf \
   -m ${RAM_SIZE_MB} -n "${IMAGE_NAME}" -s ${DISK_SIZE_MB} -u $USER -p tcp:9000:80
 EOF
