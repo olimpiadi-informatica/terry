bc4f8ef64a0cf0288c1dfa70e57fdb7d3e808fdb
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2018-02-26 23:02:46 +0100

    Added pack_sanity_check.py and some bad outputs to check the checkers

---
+++ util/sanity_checks/pack_sanity_check.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python3
+
+import argparse
+import csv
+import json
+import os.path
+import shutil
+import subprocess
+import tempfile
+import time
+import zipfile
+
+import yaml
+from colorama import Fore
+
+from terry.crypto import validate, metadata, decode, decode_data, SECRET_LEN, \
+    recover_file_password
+
+USERNAME_LEN = 6
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+
+def get_output(command, stdin=None):
+    proc = subprocess.Popen(command, stdout=subprocess.PIPE,
+                            stdin=subprocess.PIPE)
+    stdout, stderr = proc.communicate(stdin)
+    return stdout.decode()
+
+
+def get_nth_room(sede, aula):
+    char_per_aula = USERNAME_LEN - len(sede)
+    if char_per_aula < 0:
+        raise ValueError("Name too long")
+    return "%s%s" % (sede, str(aula).zfill(USERNAME_LEN - len(sede)))
+
+
+def validate_task(task, fuzz):
+    print(Fore.BLUE, "Validating task %s..." % task, Fore.RESET)
+    generator = os.path.join(task, "managers", "generator.linux.i686")
+    checker = os.path.join(task, "managers", "checker.linux.i686")
+    validator = os.path.join(task, "managers", "validator.linux.i686")
+    assert os.path.exists(generator)
+    assert os.path.exists(checker)
+    os.chmod(generator, 0o755)
+    os.chmod(checker, 0o755)
+    if not os.path.exists(validator):
+        print(Fore.YELLOW, "WARNING: Missing validator for task %s" % task,
+              Fore.RESET)
+        validator = None
+    else:
+        os.chmod(validator, 0o755)
+
+    seed = "42"
+    input = os.path.join(task, "input.txt")
+    with open(input, "w") as f:
+        print("    generating input")
+        f.write(get_output([generator, seed, "0"]))
+    if validator:
+        with open(input, "rb") as f:
+            print("    validating input")
+            get_output([validator, "0"], f.read())
+
+    if fuzz:
+        for f in os.listdir(os.path.join(HERE, "bad_outputs")):
+            path = os.path.join(HERE, "bad_outputs", f)
+            if not os.path.isfile(path):
+                continue
+            print("    checking against %s" % f, end="")
+            start = time.monotonic()
+            output = get_output([checker, input, path])
+            end = time.monotonic()
+            print(" -- %.3fs" % (end-start))
+            if end - start >= 1:
+                print(Fore.YELLOW, "WARNING: check took more than 1 second",
+                      Fore.RESET)
+            if not output:
+                raise AssertionError("Checker didn't print any json")
+            data = json.loads(output)
+            if "score" not in data:
+                raise AssertionError("Check didn't print the score")
+            if "feedback" not in data:
+                raise AssertionError("Check didn't print the feedback")
+            if "validation" not in data:
+                raise AssertionError("Check didn't print the validation")
+
+
+def validate_sedi(sedi):
+    print(Fore.BLUE, "Validating sedi...", Fore.RESET)
+    with open(sedi) as f:
+        reader = list(csv.DictReader(f, delimiter=";"))
+    tokens = set()
+    for row in reader:
+        sede = row["sede"]
+        num = int(row["aule"])
+        print("    %s" % sede)
+        with open(os.path.join("__users__", get_nth_room(sede, 1) + ".yaml")) \
+                as f:
+            contest = yaml.load(f)
+            for user in contest["users"]:
+                if user["token"] in tokens:
+                    raise AssertionError("Duplicate token: %s" % user["token"])
+                tokens.add(user["token"])
+        for aula in range(1, num + 1):
+            full_sede = get_nth_room(sede, aula)
+            if not os.path.exists(os.path.join("__users__",
+                                               "%s.yaml" % full_sede)):
+                raise AssertionError("YAML for sede %s not found" % full_sede)
+            with open(os.path.join("__users__", full_sede + ".yaml")) as f:
+                contest2 = yaml.load(f)
+                if contest != contest2:
+                    raise AssertionError("YAML for room %d is different from "
+                                         "room 1 in %s" % (aula, sede))
+
+
+def validate_admin(admin, password):
+    print(Fore.BLUE, "Validating admins...", Fore.RESET)
+    with open(admin) as f:
+        admins = list(csv.DictReader(f, delimiter=";"))
+    for admin in admins:
+        full_sede = admin["full_sede"]
+        print("    %s" % full_sede)
+        token = admin["password"]
+        if full_sede != token.split("-")[0]:
+            raise AssertionError(
+                "Token of admin %s does not starts with %s-" % (
+                    full_sede, full_sede))
+        secret, passwd = decode_data(token[len(full_sede) + 1:], SECRET_LEN)
+        if recover_file_password(full_sede, secret, passwd).hex() != password:
+            raise AssertionError("Invalid token for admin %s" % full_sede)
+
+
+def main(args):
+    with open(args.pack, "rb") as pack:
+        pack = pack.read()
+    if not validate(pack):
+        raise AssertionError("Corrupted pack")
+    meta = yaml.load(metadata(pack).strip(b"\x00"))
+    if meta.get("deletable"):
+        print(Fore.YELLOW, "WARNING: The pack is marked as deletable",
+              Fore.RESET)
+    if not meta.get("name"):
+        print(Fore.YELLOW, "WARNING: The pack metadata does not include 'name'",
+              Fore.RESET)
+    if not meta.get("description"):
+        print(Fore.YELLOW,
+              "WARNING: The pack metadata does not include 'description'",
+              Fore.RESET)
+    decoded = decode(bytes.fromhex(args.password), pack)
+
+    extract_dir = tempfile.mkdtemp()
+    os.chdir(extract_dir)
+    print("Working in %s" % extract_dir)
+
+    with open("pack.zip", "wb") as f:
+        f.write(decoded)
+    with zipfile.ZipFile("pack.zip") as zip_file:
+        zip_file.extractall(".")
+    if args.sedi:
+        validate_sedi(args.sedi)
+    if args.admin:
+        validate_admin(args.admin, args.password)
+    for task in args.tasks.split(","):
+        if not os.path.exists(task):
+            raise AssertionError("Task %s not included in the pack" % task)
+        validate_task(task, args.fuzz)
+
+    shutil.rmtree(extract_dir)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("pack", help="pack.zip.enc to check")
+    parser.add_argument("password", help="Password of the pack")
+    parser.add_argument("tasks", help="List of the task names, "
+                                      "comma separated")
+    parser.add_argument("--sedi", help="CSV with sede;aule to check the "
+                                       "users", type=os.path.abspath)
+    parser.add_argument("--admin", help="CSV with sede;full_sede;token to "
+                                        "check admin tokens",
+                        type=os.path.abspath)
+    parser.add_argument("--fuzz", help="Perform an intensive test of the "
+                                       "checker fuzzing some inputs and "
+                                       "outputs",
+                        action="store_true")
+    main(parser.parse_args())
