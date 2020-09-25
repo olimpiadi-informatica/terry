c6eef0f183e3669df937f51bec75235cae4ed684
William Di Luigi <williamdiluigi@gmail.com>
2020-09-25 22:11:51 +0200

    Update eslint config

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,27 +1,43 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es6: true,
   },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
+    "airbnb",
   ],
-  parser: "@typescript-eslint/parser",
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 12,
-    sourceType: "module",
+    ecmaVersion: 2021,
+    sourceType: 'module',
   },
-  plugins: ["react", "@typescript-eslint"],
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
   rules: {
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "no-empty-function": "off",
     "@typescript-eslint/no-empty-function": "off",
+    // we only want single quotes
+    'quotes': ['error', 'single'],
+    // we want to force semicolons
+    'semi': ['error', 'always'],
+    // we use 2 spaces to indent our code
+    'indent': ['error', 2],
+    // we want to avoid useless spaces
+    'no-multi-spaces': ['error']    
   },
   settings: {
     react: {
