b9cf40fb821394921a629b062d1a1cd32c1b3e9b
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2020-09-27 23:31:09 +0200

    Show contest title and description in the login page

--- frontend/src/contest/LoginView.tsx
+++ frontend/src/contest/LoginView.tsx
@@ -2,12 +2,15 @@ import React, { createRef, useState } from "react";
 import { Trans, t } from "@lingui/macro";
 import { i18n } from "src/i18n";
 import { Loading } from "src/Loading";
+import ReactMarkdown from "react-markdown";
+import { usePack } from "src/admin/hooks/usePack";
 import { useActions, useContest } from "./ContestContext";
 
 export function LoginView() {
   const tokenRef = createRef<HTMLInputElement>();
   const { login } = useActions();
   const contest = useContest();
+  const pack = usePack().value();
   const [isLoading, setIsLoading] = useState(false);
 
   const doLogin = () => {
@@ -17,44 +20,55 @@ export function LoginView() {
   };
 
   return (
-    <div className="jumbotron">
-      <h1 className="text-center">
-        <Trans>Please login</Trans>
-      </h1>
-      <form
-        action=""
-        onSubmit={(e) => {
-          e.preventDefault();
-          doLogin();
-        }}
-      >
-        <div className="form-group">
-          <label htmlFor="token" className="sr-only">
-            <Trans>Token</Trans>
-          </label>
-          <input
-            autoComplete="off"
-            name="token"
-            id="token"
-            ref={tokenRef}
-            className="form-control text-center"
-            required
-            placeholder={i18n._(t`Token`)}
-            type="text"
-          />
-        </div>
-        <input type="submit" className="btn btn-primary" value={i18n._(t`Login`)} />
-        {contest.isError() && (
-          <div className="alert alert-danger mt-2" role="alert">
-            <strong>
-              <Trans>Error</Trans>
-            </strong>
-            {" "}
-            {contest.error().response?.data.message}
+    <>
+      {
+        pack && pack.uploaded && (
+          <div>
+            <h1>{pack.name}</h1>
+            <ReactMarkdown source={pack.description} />
+            <hr />
           </div>
-        )}
-        {isLoading && <Loading />}
-      </form>
-    </div>
+        )
+      }
+      <div className="jumbotron">
+        <h1 className="text-center">
+          <Trans>Please login</Trans>
+        </h1>
+        <form
+          action=""
+          onSubmit={(e) => {
+            e.preventDefault();
+            doLogin();
+          }}
+        >
+          <div className="form-group">
+            <label htmlFor="token" className="sr-only">
+              <Trans>Token</Trans>
+            </label>
+            <input
+              autoComplete="off"
+              name="token"
+              id="token"
+              ref={tokenRef}
+              className="form-control text-center"
+              required
+              placeholder={i18n._(t`Token`)}
+              type="text"
+            />
+          </div>
+          <input type="submit" className="btn btn-primary" value={i18n._(t`Login`)} />
+          {contest.isError() && (
+            <div className="alert alert-danger mt-2" role="alert">
+              <strong>
+                <Trans>Error</Trans>
+              </strong>
+              {" "}
+              {contest.error().response?.data.message}
+            </div>
+          )}
+          {isLoading && <Loading />}
+        </form>
+      </div>
+    </>
   );
 }
