94e26c7c683cd04b2e64a778231a5f3fd31b4a3e
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2020-10-05 11:10:51 +0200

    Babel + tsc

--- frontend/packages/communication/src/components/Questions.tsx
+++ frontend/packages/communication/src/components/Questions.tsx
@@ -1,24 +1,27 @@
 import React from "react";
 import { useQuestions } from "src/hooks/useCommunication";
 import { Loading } from "src/terry-frontend/Loading";
-import { Question } from "./Question";
+import { Question } from "@terry/shared/_/components/Question";
+import { DateTime } from "luxon";
 
 export function Questions() {
   const questions = useQuestions();
   const notAnswered = questions.isReady() && questions.value().filter((q) => !q.answer);
   const answered = questions.isReady() && questions.value().filter((q) => q.answer);
+  const serverTime = () => DateTime.fromJSDate(new Date());
+
   return (
     <>
       <h2>Not answered</h2>
       { questions.isLoading() && <Loading />}
       { questions.isError() && "Error"}
-      { notAnswered && notAnswered.map((q) => <Question key={q.id} question={q} />)}
+      { notAnswered && notAnswered.map((q) => <Question key={q.id} question={q} serverTime={serverTime} />)}
       { notAnswered && notAnswered.length === 0 && <em>No new questions</em> }
 
       <h2 className="mt-3">Answered</h2>
       { questions.isLoading() && <Loading />}
       { questions.isError() && "Error"}
-      { answered && answered.slice().reverse().map((q) => <Question key={q.id} question={q} />)}
+      { answered && answered.slice().reverse().map((q) => <Question key={q.id} question={q} serverTime={serverTime} />)}
       { answered && answered.length === 0 && <em>No new questions</em> }
     </>
   );
