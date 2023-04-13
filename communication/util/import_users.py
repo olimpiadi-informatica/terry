5b3a51fd21a7d6159d2d4970dcdd5813d2296a94
Dario Petrillo <dario.pk1@gmail.com>
2023-04-13 20:55:48 +0200

    communication/import_users: import from terry database

--- communication/util/import_users.py
+++ communication/util/import_users.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
 
+import sys
 import csv
 import sqlite3
 import argparse
 
 
 def main(args):
-    with open(args.users) as f:
-        reader = csv.DictReader(f)
-        tokens = {(r["token"],) for r in reader}
+    if args.users is not None:
+        with open(args.users) as f:
+            reader = csv.DictReader(f)
+            tokens = {(r["token"],) for r in reader}
+    else:
+        conn = sqlite3.connect(args.terrydb)
+        cur = conn.cursor()
+        tokens = cur.execute('SELECT token FROM users').fetchall()
+        conn.close()
+
     print("Loaded %d users" % len(tokens))
 
     conn = sqlite3.connect(args.db)
@@ -25,7 +33,13 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("users", help="users.csv file with the 'token' column")
     parser.add_argument("db", help="output database of where to insert the users")
+    parser.add_argument("--users", help="users.csv file with the 'token' column")
+    parser.add_argument("--terrydb", help="terry database to read users from")
     args = parser.parse_args()
+
+    if (args.terrydb is None and args.users is None) or (args.terrydb is not None and args.users is not None):
+        print("Exactly one of --users and --terrydb must be provided", file=sys.stderr)
+        sys.exit(1)
+
     main(args)
