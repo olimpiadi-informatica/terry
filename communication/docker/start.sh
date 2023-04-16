1e096b502e9feee78722a28044347e93a2280471
Dario Petrillo <dario.pk1@gmail.com>
2023-04-16 19:59:00 +0200

    Fix standalone communication Dockerfile

---
+++ communication/docker/start.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+cd /data
+mkdir /data/logs
+
+export RUST_LOG=info
+options=""
+# fetch the telegram configuration from the env
+if [ ! -z "$TELEGRAM_TOKEN" ]; then
+    options="$options --token=$TELEGRAM_TOKEN"
+    if [ ! -z "$TELEGRAM_CHANNEL_ID" ]; then
+        options="$options --channel-id=$TELEGRAM_CHANNEL_ID"
+    else
+        echo "To enable the telegram bot pass also '-e TELEGRAM_CHANNEL_ID=-1001111111111'"
+        exit 2
+    fi
+    if [ ! -z "$TELEGRAM_ADMIN_URL" ]; then
+        options="$options --admin-url=$TELEGRAM_ADMIN_URL"
+    else
+        echo "To enable the telegram bot pass also '-e TELEGRAM_ADMIN_URL=https://somewhere.online/admin/communication'"
+        exit 2
+    fi
+fi
+
+/terry-communication-backend \
+    --database /data/terry-communication-backend.sqlite3 \
+    --bind 0.0.0.0:1236 \
+    $options 2>&1 \
+| tee -a /data/logs/terry-communication-backend.log &
